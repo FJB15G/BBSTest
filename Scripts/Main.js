2092c46fde9bd61d86c190f71ee86f3ead841498
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2016-12-06 15:05:05 +0900

    12/7(水)授業用

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -5,50 +5,56 @@ var BBS_URL = "https://script.google.com/macros/s/AKfycbwhKTCoMevjtDmWK1RUMGCi_g
 
 function Main()
 {
+  //-------------------------------------------
   //メッセージの読み込み命令
   function load(){
-    var params = {"cmd":"LIST"};
-    sendJson(BBS_URL,params,onLoad);
-  }
-  //メッセージが送られてきたら処理する内容
-  function onLoad(json){
-    var html = "";
-    var values = json.values;
-    for(var index in values){
-      var value = values[index];
-      html = "<div>"+value[2]+":"+value[3]+"</div>" + html;
+    var params = {"cmd":"LIST","id":lastId};        //送信パラメータの設定
+    sendJson(BBS_URL,params,onLoad);                //データの送信
+    //-------------------------------------------
+    //メッセージが送られてきたら処理する内容
+    //データ内容 0:ID 1:日付 2:名前 3:メッセージ
+    function onLoad(json){
+      var values = json.values;
+      for(var index in values){
+        var value = values[index];                  //配列からデータを受け取る
+        var div = document.createElement('div');    //出力用のタグを生成
+        div.innerHTML = value[2]+":"+value[3];      //メッセージを作成
+        lastId = value[0];                          //最後に受け取ったIDを保存
+        output.insertBefore(div, output.firstChild);//先頭に追加
+      }
     }
-    output.innerHTML =  html;
   }
+
+  //-------------------------------------------
   //書き込み命令
   function write(name,msg){
-    var params = {"cmd":"WRITE","name":name,"msg":msg};
-    sendJson(BBS_URL,params,onWrite);
-
-  }
-  //書き込みが完了したときに処理する内容
-  function onWrite(json){
-    if(json && json.result){
-      inputs[2].value = "送信完了";
-      load(); //書き込みが完了したあとに読み直す
+    var params = {"cmd":"WRITE","name":name,"msg":msg}; //送信パラメータの設定
+    sendJson(BBS_URL,params,onWrite);                   //データの送信
+    //-------------------------------------------
+    //書き込みが完了したときに処理する内容
+    function onWrite(json){
+      if(json && json.result){
+        inputs[2].value = "送信完了";
+        load(); //書き込みが完了したあとに読み直す
+      }
+      else
+        inputs[2].value = "送信エラー";
     }
-    else
-      inputs[2].value = "送信エラー";
   }
 
+  //-------------------------------------------
   //各インスタンスの取得
   var inputs = document.querySelectorAll("input");
   var output = document.querySelector("div");
 
   //送信ボタンを押した場合の処理
   inputs[2].onclick = function(){
-    var name = inputs[0].value;
+    var name = inputs[0].value; //名前の取得
     var msg = inputs[1].value;  //メッセージの取得
     inputs[2].value = "送信中";  //ボタン表示の変更
     write(name,msg);            //書き込み
   }
 
-  load(); //ページを開いた直後に読み込みを行う
-
-  //write("ふぉ","ふぉっふぉっふぉ");
+  var lastId = 0;   //最後に受け取ったメッセージID
+  load();           //メッセージを読み込む
 }
